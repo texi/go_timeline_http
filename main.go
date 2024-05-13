d67e01aed2a574bcfeb9a5537b14bc057f8fefea
texi <443711934@qq.com>
2024-05-13 17:33:13 +0800

    add gin

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
-    "github.com/astaxie/beego"
+    "github.com/gin-gonic/gin"
+    "net/http"
 )
 
-func main(){
-    beego.Run()
+func main() {
+    //1.创建路由
+    r := gin.Default()
+    //2.绑定路由规则，执行的函数
+    r.GET("/", func(context *gin.Context) {
+        context.String(http.StatusOK, "Hello World!")
+    })
+    //3.监听端口，默认8080
+    r.Run(":8080")
 }
